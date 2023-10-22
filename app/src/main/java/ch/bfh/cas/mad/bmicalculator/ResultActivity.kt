18ca11adcf4d5d985e206612e67a56043de2c2b5
Yves Bonjour <yves.bonjour@rethinc.ch>
2023-10-22 12:48:38 +0200

    Adds bsck button

--- app/src/main/java/ch/bfh/cas/mad/bmicalculator/ResultActivity.kt
+++ app/src/main/java/ch/bfh/cas/mad/bmicalculator/ResultActivity.kt
@@ -3,6 +3,7 @@ package ch.bfh.cas.mad.bmicalculator
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -25,11 +26,29 @@ class ResultActivity : AppCompatActivity() {
             }
 
     }
+
+    private lateinit var buttonBack: Button
+    private lateinit var textViewOutput: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+        buttonBack = findViewById(R.id.button_back)
+        textViewOutput = findViewById<TextView>(R.id.textview_bmi_output)
         val bmi = intent.getBmi() ?: throw Exception("No BMI provided")
-        val textViewOutput = findViewById<TextView>(R.id.textview_bmi_output)
         textViewOutput.text = bmi.toString()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        buttonBack.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        buttonBack.setOnClickListener(null)
     }
 }
